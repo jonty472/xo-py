b1a477ec176339ca81b36ce6987951e0f2d33b24
JontyBurden <azttorrs@gmail.com>
2022-04-16 15:12:44 +1200

    who's turn func

--- xo_gameV2.py
+++ xo_gameV2.py
@@ -1,18 +1,38 @@
 import random
 
-print(random.randrange(1,10)) 
-
 moves = []
+bot_moves = []
 
 def xo_game():
     BOARD = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    
+    whos_turn()
     for game in range(2):
-        player_move(move = input(': '))
+        if whos_turn() == 'Player':
+            player_move(move = input(': '))
+        else:
+            bot_move()
+
 
 def update_board(position, turn):
     pass
 
+
+
+def whos_turn():
+    """
+    Rolls a dice to see who begins.
+
+    Returns if XO player or bot starts
+    """
+    player_roll = random.randrange(1,6)
+    bot_roll = random.randrange(1,6)
+    if player_roll > bot_roll:
+        return 'Player'
+    elif bot_roll > player_roll:
+        return 'Bot'
+    else:
+        print('equal. roll again')
+        return whos_turn()
 
 def player_move(move):
     if move not in moves:
@@ -21,11 +41,17 @@ def player_move(move):
         print("Position Taken. Restart")
         moves.remove(move)
         return xo_game()
-    print(moves)
+    #print(moves)
 
 def bot_move():
-    pass
-
+    move = random.randint(1,9)
+    if move not in bot_moves:
+        bot_moves.append(move)
+    else:
+        bot_moves.remove(move)
+    
 xo_game()
+print('player moves', moves)
+print('bot moves', bot_moves)
 
 
