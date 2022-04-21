52d0290872237abfafc48a49d962f2c80f0ed9d3
JontyBurden <azttorrs@gmail.com>
2022-04-21 15:35:22 +1200

    refactoring main and helper functions

--- xo_gameV2.py
+++ xo_gameV2.py
@@ -1,96 +1,65 @@
 import random
 
-player_moves = []
-bot_moves = []
-move_count = 0
 
-def xo_game(move_count):
+def xo_game():
+    x_moves = []
+    o_moves = []
+
+    move_count = 0
+    moves = []
     board = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    
-    # who starts the game: player or bot?
-    #turn = whos_turn()
-    turn = 'Player'
-    print(turn)
-    players_piece = None
-    bots_piece = None
-    if turn == 'Player':
-        players_piece = 'X'
-        bots_piece = 'O'
-    else:
-        bots_piece = 'X'
-        players_piece = 'O'
-    
-    print(turn, 'player:',players_piece, 'bot:',bots_piece)
 
-    # control flow for board and moves
-    for game in range(2):
-        if players_piece == 'X':
-            move = int(input(': '))
-            board_position = player_move(move)
-            if move_count%2 == 0:
-                move_count += 1
-                player_moves.append(board_position)
-                board[int(board_position)] = 'X'
-            else:
-                move_count += 1
-                bot_moves.append(board_position)
-                board[int(board_position)] = 'O'
+    for game in range(3):
+        if move_count%2 == 0:
+            move_count+=1
+            x_move = player_move()
+            x_moves.append(x_move)
+            board[x_move] = 'X'
+            print_board(board)
+            moves.append(x_move)
 
-        elif bots_piece == 'X':
-            board_position = bot_move()
-            if move_count%2 == 0:
-                move_count += 1
-                bot_moves.append(board_position)
-                board[int(board_position)] = 'X'
-            else:
-                move_count += 1
-                player_moves.append(board_position)
-                board[int(board_position)] = 'O'
-            
-            
+        elif move_count%2 == 1:
+            move_count+=1
+            #o_move = bot_move()
+            o_move = 5
+            o_moves.append(o_move)
+            board[o_move] = 'O'
+            print('BOTS MOVE:')
+            print_board(board) 
+            moves.append(o_move)
 
-def update_board(position, turn):
-    pass
+        elif valid_move(moves) == False:
+            print('not valid move\nstart again...')
+            return xo_game()
+
+    print('x:', x_moves)
+    print('o:', o_moves)
 
 
-
-def whos_turn():
-    """
-    Rolls a dice to see who begins.
-
-    Returns if XO player or bot starts
-    """
-    player_roll = random.randrange(1,6)
-    bot_roll = random.randrange(1,6)
-    if player_roll > bot_roll:
-        turn = 'Player'
-    elif bot_roll > player_roll:
-        turn = 'Bot'
-    elif int(player_roll) == int(bot_roll):
-        print('equal. roll again')
-        return whos_turn()
-
-    return turn
-
-def player_move(move):
-    if move not in player_moves:
-        player_moves.append(move)
-    else:
-        print("Position Taken. Restart")
-        player_moves.remove(move)
-        return xo_game()
-    #print(moves)
 
 def bot_move():
-    move_count = move_count + 1
     move = random.randint(1,9)
-    if move not in bot_moves:
-        bot_moves.append(move)
-    else:
-        bot_moves.remove(move)
+    return move
+
+def player_move():
+    move = int(input(': '))
+    return move
+
+def print_board(board):
+    return print("\n{} | {} | {}\n--+---+--\n{} | {} | {}\n--+---+--\n{} | {} | {}\n".format(board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8],board[9],))
+
+def valid_move(moves):
+    """
+    Valid Move checks if a position on the board is taken.
+    Input: all moves
+    Returns True if board position not taken. False if board position already
+    taken.
+    """
+    for move in reversed(moves):
+        if moves.count(move) > 1:
+            return False
+        else:
+            return True
     
-xo_game(move_count)
-print('player moves', player_moves)
-print('bot moves', bot_moves)
 
-
+xo_game()
